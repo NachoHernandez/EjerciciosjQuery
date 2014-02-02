af280519f85e7fece43d1d6e0daddcd3d81628ac
Nacho Hernández <nachohmoreno@gmail.com>
2014-02-02 21:32:09 +0100

    añadiendo más ejemplos

---
+++ ejemplo_preguntas_frecuentes/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+ $('.par').toggle();
+  $('#tit1').click(function(){
+    
+  $('#par1').toggle(300);
+    
+  })
+  $('#tit2').click(function(){
+    $('#par2').toggle(300);
+    })
+})
